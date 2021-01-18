7260a6e8328d904f9202cb27aef56e247c454ecb
CJ McCaskill <cjmccaskill@gmail.com>
2021-01-18 14:53:57 -0600

    Add function and mark up in multiple js files

---
+++ functional.js
@@ -0,0 +1,102 @@
+let states = [
+  "Oklahoma",
+  "Texas",
+  "Georgia",
+  "Alabama",
+  "North Dakota",
+  "South Dakota",
+];
+
+// // MAPS
+
+// // Returns a URL-friendly version of a string.
+// // Example: "South Dakota" -> "south-dakota"
+// function urlify(string) {
+//   return string.toLowerCase().split(/\s+/).join("-");
+// }
+
+// // urls: Imperative version
+// function imperativeUrls(elements) {
+//   let urls = [];
+//   elements.forEach(function (element) {
+//     urls.push(urlify(element));
+//   });
+//   return urls;
+// }
+// console.log(imperativeUrls(states));
+
+// // urls: Functional version
+// function functionalUrls(elements) {
+//   return elements.map((element) => urlify(element));
+// }
+// console.log(functionalUrls(states));
+
+// // FILTER
+
+// // singles: Imperative version
+// function imperativeSingles(elements) {
+//   let singles = [];
+//   elements.forEach(function (element) {
+//     if (element.split(/\s+/).length === 1) {
+//       singles.push(element);
+//     }
+//   });
+//   return singles;
+// }
+// console.log(imperativeSingles(states));
+
+// //  singles: Functional version
+// function functionalSingles(elements) {
+//   return elements.filter((element) => element.split(/\s+/).length === 1);
+// }
+// console.log(functionalSingles(states));
+
+// REDUCE
+
+let numbers = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10];
+
+// sum: Imperative solution
+function imperativeSum(elements) {
+  let total = 0;
+  elements.forEach(function (n) {
+    total += n;
+  });
+  return total;
+}
+console.log(imperativeSum(numbers));
+
+// sum: functional solution
+function functionalSum(elements) {
+  return elements.reduce((total, n) => {
+    return (total += n);
+  });
+}
+console.log(functionalSum(numbers));
+
+// LENGTH REDUCE
+
+// lengths: Imperative solution
+function imperativeLengths(elements) {
+  let lengths = {};
+  elements.forEach(function (element) {
+    lengths[element] = element.length;
+  });
+  return lengths;
+}
+console.log(imperativeLengths(states));
+
+// lengths: Functional solution
+function functionalLengths(elements) {
+  return elements.reduce((lengths, element) => {
+    lengths[element] = element.length;
+    return lengths;
+  }, {});
+}
+console.log(functionalLengths(states));
+
+function functionalProduct(elements) {
+  return elements.reduce((total, n) => {
+    return (total *= n);
+  });
+}
+console.log(functionalProduct(numbers));
